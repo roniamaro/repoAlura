b133b4092cc15e1a78794c7930970f4c8b3391c0
roni_amaro <roni_amaro@hotmail.com>
2024-03-08 15:19:10 -0300

    Finalização do projeto Jogo Secreto 2.0

--- 02 Explore funções e listas/projeto_02/app.js
+++ 02 Explore funções e listas/projeto_02/app.js
@@ -1,9 +1,65 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let listaDeNumSorteado[];
+let numMax = 10;
+let numSecreto = gerarNumSecreto();
+let count = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function gerarNumAleatorio() {
+  let numeroGerado = parseInt(Math.random() * numMax + 1);
+  let quantidadeDeElementosNaLista = listaDeNumSorteado.length;
+
+  if (quantidadeDeElementosNaLista == numMax ) {
+    listaDeNumSorteado = [];    
+  }
+
+  if (listaDeNumSorteado.includes(numeroGerado)) {
+    return gerarNumAleatorio();
+  } else {
+    listaDeNumSorteado.push(numeroGerado); //push insere no final da lista
+    return numeroGerado;
+  }
+}
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+  responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela('h1', 'Jogo do número secreto!');
+  exibirTextoNaTela('p', 'Escolha um número entre 0 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
-  
-}
+  let chute = document.querySelector('input').value;
+
+  if (chute == numSecreto) {
+    let palavraTentativa = count > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Parabéns você descobriu o número secreto com ${count} ${palavraTentativa}.`;
+    exibirTextoNaTela('h1', 'Acertou!');
+    exibirTextoNaTela('p', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disable');
+  } else {
+    if (chute > numSecreto) {
+      exibirTextoNaTela('p', 'O número secreto é menor');      
+    } else {
+      exibirTextoNaTela('p', 'O número secreto é maior');
+    }
+    count++;
+  }
+};
+
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = '';
+};
+
+function reiniciarJogo() {
+  numSecreto = gerarNumAleatorio();
+  limparCampo();
+  count = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disable', true);
+};
--- README.md
+++ README.md
@@ -6,8 +6,5 @@ Um repositório criado para armazenar os projetos desenvolvidos em aula.
   * Live Server
 
   - A partir do zero: iniciante em programação:
-    - Lógica de programação: mergulhe em programação com JavaScript
-
-  - Aprenda a programar em JavaScript com foco no back-end:
-
-  - APIs com Node.js e Express
+    - Lógica de programação: mergulhe em programação com JavaScript;
+    - Explore funções e listas;
